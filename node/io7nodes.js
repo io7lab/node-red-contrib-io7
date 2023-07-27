1d9d2239a1ccfce3a03baede7513d4f8a0601b5c
Yoonseok Hur <yoonseok@gmail.com>
2023-07-27 14:40:49 +0900

    draft #1

--- node/io7nodes.js
+++ node/io7nodes.js
@@ -10,14 +10,14 @@ module.exports = function(RED) {
         const mqtt_url = `mqtts://${config.host}:${config.port}`;
 
         node.hubConn = mqtt.connect(mqtt_url, {
-            username: config.appid,
-            clientId: config.appid,
-            password: config.token,
+            username: config.apiKey,
+            clientId: config.apiKey,
+            password: config.apiToken,
             clean: true,
             rejectUnauthorized: false
         });
         node.hubConn.on('connect', function (topic, message) {
-            node.log(`Connected to io7 hub: ${mqtt_url} with ${config.appid}`);
+            node.log(`Connected to io7 hub: ${mqtt_url} with ${config.apiKey}`);
         });
         node._closeCallbacks.push(function() {
             node.hubConn.end();
@@ -32,7 +32,8 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         const node = this;
         node.status({fill:"yellow",shape:"ring",text:"node-red:common.status.connecting"});
-        let hubConn = RED.nodes.getNode(config.apikey).hubConn;
+        let hubConn = RED.nodes.getNode(config.authentication).hubConn;
+        node.fmt = ( node.allFormats ) ? '+' : config.fmt;
 
         hubConn.on('connect', function (topic, message) { hubConn.subscribe(`iot3/${config.devid}/evt/${config.evt}/fmt/${config.fmt}`);
             node.status({fill:"green",shape:"dot",text:"node-red:common.status.connected"});
@@ -52,7 +53,7 @@ module.exports = function(RED) {
         const node = this;
         node.status({fill:"yellow",shape:"ring",text:"node-red:common.status.connecting"});
 
-        let hub = RED.nodes.getNode(config.apikey);
+        let hub = RED.nodes.getNode(config.authentication);
         if (hub == null || hub.hubConn == null) {
             return;
         }
